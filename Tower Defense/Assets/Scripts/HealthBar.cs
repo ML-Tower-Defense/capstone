709072e39d4d4fd5b21ea8f2f14cc76b77f77649
Steven Bui <bui.stvn@gmail.com>
2022-01-27 17:21:10 -0800

    Adjust position

--- Tower Defense/Assets/Scripts/HealthBar.cs
+++ Tower Defense/Assets/Scripts/HealthBar.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
- public Slider slider;
+    public Slider slider;
 
     public void setMaxHealth(int health){
         slider.maxValue = health;
