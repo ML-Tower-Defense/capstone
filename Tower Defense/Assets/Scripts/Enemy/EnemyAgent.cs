5678b5c4970ef27be6f1d5901d259ee4bddfdf45
Steven Bui <bui.stvn@gmail.com>
2022-05-08 23:20:24 -0700

    Add base variables and functions for attacking

--- Tower Defense/Assets/Scripts/Enemy/EnemyAgent.cs
+++ Tower Defense/Assets/Scripts/Enemy/EnemyAgent.cs
@@ -10,17 +10,61 @@ public class EnemyAgent : Agent
     // Reinforcement learning cycle
     // Observation -> Decision -> Action -> Reward
 
+    private float rotationSpeed = 3f;
+    private bool isAttackReady = true;
+    private int attackCooldown = 0;
+    private string towerTag = "Tower";
+
+    // Updates the attack cooldown to determine if attack is ready
+    void Update()
+    {
+        if (!isAttackReady)
+        {
+            attackCooldown -= 1;
+
+            if (attackCooldown <= 0)
+            {
+                isAttackReady = true;
+            }
+        }
+    }
+
+    private void AttackTower()
+    {
+        // Return and do nothing if attack is still on cooldown
+        if (!isAttackReady)
+        {
+            return;
+        }
+
+        // Shoot a projectile in the forward direction
+        // If projectile hits the most valuable tower, add a high positive reward
+        // Else if projectile hits any other tower, add a low positive reward
+        // Else (projectile misses), add a negative reward
+
+        // Set attack to not ready and reset the cooldown
+        isAttackReady = false;
+        attackCooldown = 10;
+    }
+
     public override void CollectObservations(VectorSensor sensor)
     {
-        // Agent takes in its own position as observation data
-        sensor.AddObservation(transform.position);
-
-        // Agent takes in positions of existing towers on the map?
+        // Agent takes in positions, kill counts, and projectile counts of towers
+        GameObject[] towers = GameObject.FindGameObjectsWithTag(towerTag);
+        foreach (GameObject tower in towers)
+        {
+            Tower towerScript = tower.GetComponent<Tower>();
+            sensor.AddObservation(tower.transform.position);
+            sensor.AddObservation(towerScript.killCount);
+            sensor.AddObservation(towerScript.projectileCount);
+        }
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        /*
         int dec = actions.DiscreteActions[0];
+
         //if 0, move
         if (dec == 0) {
             if (this.gameObject.GetComponent<EnemyMovement>().enabled) {
@@ -40,6 +84,17 @@ public class EnemyAgent : Agent
 
             }
         }
+        */
+
+        float readyToShoot = actions.ContinuousActions[0]; 
+        float rotationDegree = actions.ContinuousActions[1];
+
+        if (readyToShoot >= 1)
+        {
+            AttackTower();
+        }
+
+        transform.Rotate(Vector3.forward, rotationDegree * rotationSpeed);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut) {
