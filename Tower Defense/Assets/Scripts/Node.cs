e5cf948bff7dcaf89a81a274da410afff25b1b24
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2022-05-29 00:57:01 -0700

    Balanced gold received from selling towers
    - Made selling towers refund about 25% of their cost
    - Updated delete tower prompt accordingly for each tower to reflect this

--- Tower Defense/Assets/Scripts/Node.cs
+++ Tower Defense/Assets/Scripts/Node.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Node : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class Node : MonoBehaviour
 
     private static GameObject whichNode;
     private GameObject childTower;
+
+    private static int goldRefund;                 // Gold received from selling a tower
 
     AudioManager audioManager;
 
@@ -124,7 +127,7 @@ public class Node : MonoBehaviour
 
     public void deleteTower()
     {
-        money.AddGold(100);
+        money.AddGold(goldRefund);
 
         try
         {
@@ -142,6 +145,25 @@ public class Node : MonoBehaviour
     {
         //print("Open delete tower menu");
         BuildMenu.GameBuildSingle = false;
+
+        switch (whichNode.transform.GetChild(0).gameObject.name)
+        {
+            case "DarkMageTower(Clone)":
+                deleteTowerPrompt.GetComponentInChildren<TextMeshProUGUI>().text = "Do you want to sell tower for     40?";
+                goldRefund = 40;
+                break;
+
+            case "ArcherTower(Clone)":
+                deleteTowerPrompt.GetComponentInChildren<TextMeshProUGUI>().text = "Do you want to sell tower for     50?";
+                goldRefund = 50;
+                break;
+
+            case "DragonTower(Clone)":
+                deleteTowerPrompt.GetComponentInChildren<TextMeshProUGUI>().text = "Do you want to sell tower for     75?";
+                goldRefund = 75;
+                break;
+        }
+
         deleteTowerPrompt.SetActive(true);
     }
 
